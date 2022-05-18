02b5e8177769c9b3db54e2b4d7153bd96f1b613d
brunaalvesws <baws@cin.ufpe.br>
2022-05-18 16:08:41 -0300

    ajustes nas consultas de bruna

--- consultas.js
+++ consultas.js
@@ -1,18 +1,13 @@
 // Consultas de Bruna 
-/*- [ ] GTE
-- [ ] EXISTS
-- [ ] PRETTY
-- [ ] UPDATE
-- [ ] ADDTOSET */
-
 // Update e add to set - Atualizar o combo vegeteriano para conter um suco nos seus produtos
 db.combos.update(
-    { _id: "fgm3" },
+    { id_combo: "fgm3" },
     { $addToSet: { produtos:  db.produtos.findOne({id_produto: "777b"})._id } }
 );
+db.combos.find({ id_combo: "fgm3" }).pretty();
 
 //gte e pretty - produtos que possuem preço maior que 30 reais, formatados na saída
 db.produtos.find({ preco: { $gte: 30.00 } } ).pretty();
 
 //exists - verificar quais produtos veganos existem no cardápio
-db.produtos.find({ veg : {$exists: true } });
+db.produtos.find({ veg : {$exists: true } }).pretty();
